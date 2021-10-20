fe3de61c0fb2fbc84be9aa5df94139c1672eeda3
rakeshavm <rakesh.avm@gmail.com>
2021-10-20 17:39:43 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name='Track My Dollar',
-    version='1.0',
-    description='An easy to use Telegram Bot to track everyday expenses',
-    author='Dev, Prakruthi, Radhika, Rohan, Sunidhi',
-    scripts=['code/code.py'],
-    packages=find_packages()
-)   
+    name='MyDollarBot',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Telegram bot to manage expenses',
+    tests_require=['pytest'],
+    author='CSC510 - Group 21',
+    author_email='mydollarbot@gmail.com',
+    url='https://github.com/mtkumar123/MyDollarBot',
+    python_requires='>=3.7',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: MyDollarBot",
+    ],
+    license='MIT',
+    install_requires=[
+        'pyTelegramBotAPI==4.1.0'
+    ]
+)
