60882d060546b8f50d6442428dc07f3f85885d91
rakeshavm <rakesh.avm@gmail.com>
2021-11-01 11:46:58 -0400

    Update add_monthly_budget

--- code/user.py
+++ code/user.py
@@ -241,8 +241,9 @@ class User:
         :param userid:
         :return:
         """
-        self.monthly_budget = amount
-        self.save_user(userid)
+        if amount != 0:
+            self.monthly_budget = amount
+            self.save_user(userid)
 
     def monthly_total(self):
         """
