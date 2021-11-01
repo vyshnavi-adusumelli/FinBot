68130c0aad25aa273127d7b96a306cd44fe1d2e9
rakeshavm <rakesh.avm@gmail.com>
2021-11-01 11:41:38 -0400

    Add monthly budget test

---
+++ test/unit/test_add_monthly_budget.py
@@ -0,0 +1,35 @@
+"""
+Tests the add_monthly_budget_method
+"""
+
+from BaseCase import BaseCase
+
+
+class TestAddMonthlyBudget(BaseCase):
+    """
+    Unit test for add monthly budget
+    """
+    def add_monthly_budget_valid(self):
+        """
+        Asserts when add_monthly_budget is given a float value
+
+        """
+        assert self.user.monthly_budget == 0
+        amount = 10.00
+        self.user.add_monthly_budget(amount, 1)
+        assert self.user.monthly_budget == amount
+
+    def add_monthly_budget_invalid(self):
+        """
+        Asserts when add_monthly_budget is given 0
+
+        """
+        assert self.user.monthly_budget == 0
+        amount_valid = 10.00
+        self.user.add_monthly_budget(amount_valid, 1)
+        assert self.user.monthly_budget == amount
+        amount = 0.00
+        self.user.add_monthly_budget(amount, 1)
+        assert self.user.monthly_budget == amount_valid
+
+
