240b5291b972f059c657b777d00adc9bb3a9bff7
Manoj Kumar <mtkumar@ncsu.edu>
2021-11-02 20:42:09 -0400

    fix test

--- test/bot/test_bot.py
+++ test/bot/test_bot.py
@@ -21,7 +21,7 @@ class TestCommands(BotTest):
         # for all commands
         bot_commands = [hand for hand in self.bot.message_handlers
                         if "commands" in hand['filters']]
-        number_of_commands = 7
+        number_of_commands = 8
         # assert there is the right number of commands
         assert len(bot_commands) == number_of_commands
 
