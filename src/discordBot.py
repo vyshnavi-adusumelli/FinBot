55b801a18fda1a8c910c54b1e6c3368de1ed9168
vadusum <vadusum@ncsu.edu>
2023-10-04 12:27:16 -0400

    Calendar menu for tasks add, edit, delete

--- src/discordBot.py
+++ src/discordBot.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from discord.ext import commands, tasks
 import discord
 from discordUser import User
@@ -25,18 +27,55 @@ CHANNEL_ID = os.environ["CHANNEL_ID"]
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 user_list = {}
 
+
 @bot.event
 async def on_ready():
     print("Hello! FinBot is ready")
     channel = bot.get_channel(int(CHANNEL_ID))
     await channel.send("Hello! Welcome to FinBot!")
 
+
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello!")
 
+async def select_date(ctx):
+    await ctx.send("Enter the date (1-31):")
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    try:
+        date_message = await bot.wait_for('message', check=check, timeout=60)
+        date = date_message.content.strip()
+
+        await ctx.send("Enter the month (1-12):")
+        month_message = await bot.wait_for('message', check=check, timeout=60)
+        month = month_message.content.strip()
+
+        await ctx.send("Enter the year (e.g., 2023):")
+        year_message = await bot.wait_for('message', check=check, timeout=60)
+        year = year_message.content.strip()
+
+        # Call the next function with the date, month, and year
+        await process_date(ctx, date, month, year)
+    except asyncio.TimeoutError:
+        await ctx.send("You took too long to respond. Please try again.")
+
+async def process_date(ctx, date, month, year):
+    # Process the date, month, and year here
+    # You can perform any necessary calculations or operations
+    # For example, you can convert them to a datetime object
+    try:
+        date_obj = datetime(int(year), int(month), int(date))
+        await ctx.send(f"Selected Date: {date_obj.strftime('%Y-%m-%d')}")
+        await select_category(ctx, bot, date_obj)
+    except ValueError:
+        await ctx.send("Invalid date, month, or year. Please enter valid values.")
+
+async def select_category(ctx, bot, date):
+    await ctx.send("Hello!")
 @bot.command()
-async def add(ctx, date, category, amount):
+async def add(ctx):
     '''
     Category should be spelled exactly matching with one of the below:
     "Food",
@@ -50,8 +89,13 @@ async def add(ctx, date, category, amount):
     '''
     if CHANNEL_ID not in user_list.keys():
         user_list[CHANNEL_ID] = User(CHANNEL_ID)
-    user_list[CHANNEL_ID].add_transaction(date,category,amount,CHANNEL_ID)
-    await ctx.send("transaction added!")
+    try:
+        await select_date(ctx)
+
+    except Exception as ex:
+        print("Exception occurred : ")
+        print(str(ex), exc_info=True)
+        await ctx.send("Processing Failed - \nError : " + str(ex))
 
 def get_users():
     """
@@ -73,9 +117,11 @@ def get_users():
                     users[u] = pickle.load(f)
     return users
 
+
 if __name__ == "__main__":
     try:
         user_list = get_users()
         bot.run(BOT_TOKEN)
     except Exception as e:
         print(f"{e}")
+
