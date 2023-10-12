cc3ad5abd14f0a9a86cfdef456b0458827f046e0
hbandar <vardha18harsha@gmail.com>
2023-10-12 12:12:29 -0400

    write test cases for discord and telebot : budget utils functions

--- test/unit/test_add_monthly_budget.py
+++ test/unit/test_add_monthly_budget.py
@@ -1,37 +1,26 @@
 """
 Tests the add_monthly_budget_method
 """
-import unittest
-from BaseCase import BaseCase
+from discord_BaseCase import discord_BaseCase
 
-
-class TestAddMonthlyBudget(BaseCase):
-    """
-    Unit test for add monthly budget
-    """
-    def add_monthly_budget_valid(self):
+class TestAddMonthlyBudget(discord_BaseCase):
+    def test_add_monthly_budget_valid(self):
         """
         Asserts when add_monthly_budget is given a float value
-
         """
         assert self.user.monthly_budget == 0
         amount = 10.00
-        self.user.add_monthly_budget(amount, 1)
+        self.user.add_monthly_budget(amount, 2)
         assert self.user.monthly_budget == amount
 
-    def add_monthly_budget_invalid(self):
+    def test_add_monthly_budget_invalid(self):
         """
         Asserts when add_monthly_budget is given 0
-
         """
         assert self.user.monthly_budget == 0
         amount_valid = 10.00
-        self.user.add_monthly_budget(amount_valid, 1)
-        assert self.user.monthly_budget == amount
-        amount = 0.00
-        self.user.add_monthly_budget(amount, 1)
+        self.user.add_monthly_budget(amount_valid, 2)
         assert self.user.monthly_budget == amount_valid
-
-
-if __name__ == '__main__':
-    unittest.main()
+        amount = 0.00
+        self.user.add_monthly_budget(amount, 2)
+        assert self.user.monthly_budget == amount_valid
--- test/unit/test_discord_monthly_budget.py
+++ test/unit/test_discord_monthly_budget.py
@@ -1,34 +1,23 @@
-"""
-Tests the add_monthly_budget_method
-"""
 from discord_BaseCase import discord_BaseCase
 
-
 class TestAddMonthlyBudget(discord_BaseCase):
-    """
-    Unit test for add monthly budget
-    """
-    def add_monthly_budget_valid(self):
+    def test_add_monthly_budget_valid(self):
         """
         Asserts when add_monthly_budget is given a float value
-
         """
         assert self.user.monthly_budget == 0
         amount = 10.00
         self.user.add_monthly_budget(amount, 2)
         assert self.user.monthly_budget == amount
 
-    def add_monthly_budget_invalid(self):
+    def test_add_monthly_budget_invalid(self):
         """
         Asserts when add_monthly_budget is given 0
-
         """
         assert self.user.monthly_budget == 0
         amount_valid = 10.00
         self.user.add_monthly_budget(amount_valid, 2)
-        assert self.user.monthly_budget == amount
+        assert self.user.monthly_budget == amount_valid
         amount = 0.00
         self.user.add_monthly_budget(amount, 2)
         assert self.user.monthly_budget == amount_valid
-
-
