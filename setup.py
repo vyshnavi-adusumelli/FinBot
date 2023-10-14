ded74c8575b728699ee803100534756f6c6c76f2
vadusum <vadusum@ncsu.edu>
2023-10-14 19:45:53 -0400

    code cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SlashBot',
+    name='FinBot',
     version='1.0.0',
     packages=find_packages(),
-    description='Telegram bot to manage expenses',
+    description='Telegram/Discord bot to manage expenses',
     tests_require=['pytest'],
-    author='CSC510 - Group 3',
+    author='CSC510 - Group 13',
     author_email='mydollarbot@gmail.com',
     url='https://github.com/secheaper/slashbot/',
     python_requires='>=3.7',
@@ -21,5 +21,6 @@ setup(
         'pyTelegramBotAPI==4.1.0',
         'pandas==1.3.4',
         'matplotlib==3.4.3'
+        'discord.py==2.3.2'
     ]
 )
